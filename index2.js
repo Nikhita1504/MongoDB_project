1d9ad9d395fe64d2df6f4606771d9ef78ddb1154
Nikhita1504 <136711092+Nikhita1504@users.noreply.github.com>
2024-08-24 18:30:58 +0530

    2commit

--- index2.js
+++ index2.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const Chat = require('./models/chat.js')
-const methodOverride = require('method-override')
+const methodOverride = require('method-override')//for using put , patch,delete
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs")
@@ -21,6 +21,8 @@ main().then((res) =>
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
+
+//crud operations
 
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find()
